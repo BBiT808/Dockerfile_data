1343c0862ae7f5a41cda6392f89765583f5ece0e
BBiT808 <witchsparadise11003311@gmail.com>
2025-08-24 21:51:54 +0900

    Update Projects.jsx

--- dev_nodejs_react/VITE_react/portfolio/src/components/Projects.jsx
+++ dev_nodejs_react/VITE_react/portfolio/src/components/Projects.jsx
@@ -21,7 +21,7 @@ useEffect(() => {
         <div className="project-description">
           <h3>직관적인 UI를 가진 도서 관리 시스템</h3>
           <p>
-            전자 도서관 시스템을 구현한 미니 팀 프로젝트로써<br />책 검색, 대출/반납, 사용자 관리 기능 등을<br />포함했습니다.
+            전자 도서관 시스템을 구현한<br />미니 팀 프로젝트로써 책 검색, 대출/반납,<br />사용자 관리 기능 등을<br />포함했습니다.
           </p>
           <div className="project-links">
             <a href="https://github.com/jszxro/PK_miniproject_3" className="btn-outline">See Live</a>
@@ -37,7 +37,7 @@ useEffect(() => {
         <div className="project-description">
           <h3>부산 지역별 최저가 상품 찾기<br/>웹 서비스</h3>
           <p>
-            부산광역시의 7개 구(남구, 동래구, 부산진구, 북구,<br /> 사상구, 사하구, 해운대구)의 소비자 물가 데이터를<br /> 분석하고 시각화하여 보여주는 웹서비스 입니다.
+            부산광역시의 7개 구(남구, 동래구, 부산진구,<br />북구, 사상구, 사하구, 해운대구)의<br />소비자 물가 데이터를 분석하고<br />시각화하여 보여주는 웹서비스 입니다.
           </p>
           <div className="project-links">
             <a href="https://github.com/BSEom/teamP_t1/tree/main" className="btn-outline">See Live</a>
@@ -53,7 +53,7 @@ useEffect(() => {
         <div className="project-description">
           <h3>Github main</h3>
           <p>
-            교육 동안 배운 기술 스택 각 종류별로 정리했습니다.<br />다양한 기술을 연마할 수 있었던<br />소중한 기간이었습니다.
+            교육 동안 배운 기술 스택을<br />각 종류별로 정리했습니다.<br />다양한 기술을 연마할 수 있었던<br />소중한 기간이었습니다.
           </p>
           <div className="project-links">
             <a href="https://github.com/BBiT808" className="btn-outline">See Live</a>
